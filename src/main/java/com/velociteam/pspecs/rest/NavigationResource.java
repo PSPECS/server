7a492c70b1e115877d7d56829e269c5783326d10
mllave <mllave@hexacta.com>
2016-06-23 18:31:49 -0300

    Se define el tipo de dato que se recibe.

--- src/main/java/com/velociteam/pspecs/rest/NavigationResource.java
+++ src/main/java/com/velociteam/pspecs/rest/NavigationResource.java
@@ -3,6 +3,7 @@ package com.velociteam.pspecs.rest;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.velociteam.pspecs.dto.NavigationDTO;
 
 @RestController
-@RequestMapping(value = "/navegacion")
+@RequestMapping(value = "/navegacion",consumes=MediaType.APPLICATION_JSON_VALUE)
 public class NavigationResource {
 	
 	@RequestMapping(method = RequestMethod.POST)

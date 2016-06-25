61a9175fbc3398c8f3b1da2a3720540b509066cd
delallavemartin <delalallave.martin@gmail.com>
2016-06-24 22:47:59 -0300

    Se agrego mongoDB y se crea la coleccion de navegacion.

--- src/main/java/com/velociteam/pspecs/dao/NavigationDAO.java
+++ src/main/java/com/velociteam/pspecs/dao/NavigationDAO.java
@@ -1,14 +1,25 @@
 package com.velociteam.pspecs.dao;
 
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Repository;
 
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.util.JSON;
 import com.velociteam.pspecs.dto.NavigationDTO;
 
 @Repository
 public class NavigationDAO {
 	
-	public void save(NavigationDTO navigationDTO){
-		//TODO
+	public void save(NavigationDTO navigationDTO) throws UnknownHostException{
+		DB db = new MongoClient(new MongoClientURI("mongodb://admin:uNckDSYqc-FL@127.8.107.130:27017/")).getDB("pspecs");
+		
+		DBObject dbObject=(DBObject) JSON.parse(JSON.serialize(navigationDTO));
+		
+		db.getCollection("navegacion").insert(dbObject);
 	}
 
 }
--- src/main/java/com/velociteam/pspecs/dto/NavigationDTO.java
+++ src/main/java/com/velociteam/pspecs/dto/NavigationDTO.java
@@ -1,6 +1,5 @@
 package com.velociteam.pspecs.dto;
 
-import java.util.Date;
 import java.util.List;
 
 public class NavigationDTO {

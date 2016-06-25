8c1e1e70427f6f2e0932429db24c976a9160fb21
delallavemartin <delalallave.martin@gmail.com>
2016-06-24 23:13:12 -0300

    Se agrego el mapeo del dto a mongo.

--- src/main/java/com/velociteam/pspecs/dao/NavigationDAO.java
+++ src/main/java/com/velociteam/pspecs/dao/NavigationDAO.java
@@ -1,14 +1,15 @@
 package com.velociteam.pspecs.dao;
 
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.util.JSON;
 import com.velociteam.pspecs.dto.NavigationDTO;
 
 @Repository
@@ -17,9 +18,16 @@ public class NavigationDAO {
 	public void save(NavigationDTO navigationDTO) throws UnknownHostException{
 		DB db = new MongoClient(new MongoClientURI("mongodb://admin:uNckDSYqc-FL@127.8.107.130:27017/")).getDB("pspecs");
 		
-		DBObject dbObject=(DBObject) JSON.parse(JSON.serialize(navigationDTO));
+		List<BasicDBObject> pictogramasMango = navigationDTO.getPictogramas().stream().map(p-> new BasicDBObject("nombre",p.getNombre())
+				.append("categoria", p.getCategoria())
+				.append("usos", p.getUsos())).collect(Collectors.toList());
 		
-		db.getCollection("navegacion").insert(dbObject);
+		db.getCollection("navegacion").insert(new BasicDBObject("dtInicio", "")
+				.append("dtFin", navigationDTO.getDtFin())
+				.append("usosOk", navigationDTO.getUsosOk())
+				.append("usosNoOk",navigationDTO.getUsosNoOk())
+				.append("usuario", navigationDTO.getUsuario())
+				.append("pictogramas", pictogramasMango));
 	}
 
 }

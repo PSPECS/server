e653a1eeba7a26ee76d0d8fda500f97e19f1a2b9
delallavemartin <delalallave.martin@gmail.com>
2016-08-01 00:46:23 -0300

    Se ajusto la query para obtener contactos

--- src/main/java/com/velociteam/pspecs/dao/UsuariosDao.java
+++ src/main/java/com/velociteam/pspecs/dao/UsuariosDao.java
@@ -4,13 +4,13 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.types.BasicBSONList;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -23,17 +23,20 @@ public class UsuariosDao {
 		List<UsuarioDTO> contactos = new ArrayList<>();
 		
 		final DB db = new MongoClient(new MongoClientURI("mongodb://admin:uNckDSYqc-FL@127.8.107.130:27017/")).getDB("pspecs");
-		DBObject dbUsuario = db.getCollection("usuario")
-				.findOne(new BasicDBObject("_id",new ObjectId(userId)),new BasicDBObject("contactos",1));
+		DBCursor dbUsuario = db.getCollection("usuario")
+				.find(new BasicDBObject("_id",new ObjectId(userId)),new BasicDBObject("contactos",1));
 		
-		BasicDBList contacts = (BasicDBList) dbUsuario.get("contacts");
-		for (Object contact : contacts) {
-			UsuarioDTO usuarioDTO = new UsuarioDTO();
-			usuarioDTO.setUsuario((String) ((DBObject) contact).get("username"));
-			usuarioDTO.setNombre((String) ((DBObject) contact).get("nombre"));
-			usuarioDTO.setApellido((String) ((DBObject) contact).get("apellido"));
-			usuarioDTO.setImagenDePerfil((String) ((DBObject) contact).get("imagenPerfile"));
-			contactos.add(usuarioDTO);
+		for (DBObject usuario : dbUsuario) {
+			BasicDBList contacts = (BasicDBList) usuario.get("contacts");
+			
+			for (BasicDBObject contact : contacts.toArray(new BasicDBObject[0])) {
+				UsuarioDTO usuarioDTO = new UsuarioDTO();
+				usuarioDTO.setUsuario((String) contact.get("username"));
+				usuarioDTO.setNombre((String) contact.get("nombre"));
+				usuarioDTO.setApellido((String) contact.get("apellido"));
+				usuarioDTO.setImagenDePerfil((String) contact.get("imagenPerfile"));
+				contactos.add(usuarioDTO);
+			}
 		}
 		return contactos;
 	}

1c2bd1fc8eb8ab1a5ef6279831cd07a2fc6a1c2f
delallavemartin <delalallave.martin@gmail.com>
2016-08-07 19:18:43 -0300

    FB instance cambiada

--- src/main/java/com/velociteam/pspecs/services/FirebaseChatService.java
+++ src/main/java/com/velociteam/pspecs/services/FirebaseChatService.java
@@ -14,30 +14,26 @@ import com.velociteam.pspecs.dto.MensajeDTO;
 @Service
 public class FirebaseChatService {
 	
-	private void initialiaze() throws FileNotFoundException{
+	private DatabaseReference initialiaze() throws FileNotFoundException{
 		FirebaseOptions options = new FirebaseOptions.Builder()
 			    .setDatabaseUrl("https://pspecs-2d21b.firebaseio.com")
 			    .setServiceAccount(new FileInputStream("pspecs-97e1d9e5648f.json"))
 			    .build();
-			FirebaseApp.initializeApp(options);
+		FirebaseApp.initializeApp(options);
+		return FirebaseDatabase
+				.getInstance()
+				.getReference("mensajes");
 	}
 	
-	private DatabaseReference getDB(){
-		return FirebaseDatabase
-			    .getInstance()
-			    .getReference("mensajes");
-	}
 	
 	public void saveMsg(String userId, MensajeDTO msg){
 		try {
-			this.initialiaze();
+			DatabaseReference dbRef = this.initialiaze().child("mensajes").child(userId);
+			dbRef.setValue(msg);
 		} catch (FileNotFoundException e) {
 			new RuntimeException("Archivo firebase no encontrado",e.getCause()); 
 		}
 		
-		DatabaseReference dbRef = getDB().child("mensajes").child(userId);
-		
-		dbRef.setValue(msg);
 		
 	}
 }

ec0e1f98d5d2dfcfe0e3e017b57870d3d6a2525a
delallavemartin <delalallave.martin@gmail.com>
2016-08-20 18:06:56 -0300

    Se usa un solo cliente para todas las conexiones a la base.

--- src/main/java/com/velociteam/pspecs/dao/AbstractDao.java
+++ src/main/java/com/velociteam/pspecs/dao/AbstractDao.java
@@ -8,14 +8,19 @@ import com.mongodb.MongoClientURI;
 
 public abstract class AbstractDao {
 	
+	private DB db = null;
+	
 	protected DB getDB() {
-		DB db = null;
-		try {
-			db = new MongoClient(new MongoClientURI("mongodb://admin:uNckDSYqc-FL@127.8.107.130:27017/")).getDB("pspecs");
-		} catch (UnknownHostException e) {
-			throw new RuntimeException(e.getCause());
+		if (db!=null){
+			return db;
+		} else{
+			try {
+				db = new MongoClient(new MongoClientURI("mongodb://admin:uNckDSYqc-FL@127.8.107.130:27017/")).getDB("pspecs");
+			} catch (UnknownHostException e) {
+				throw new RuntimeException(e.getCause());
+			}
+			return db;
 		}
-		return db;
+		
 	}
-
 }

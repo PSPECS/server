3dbe1850fc69d54853eb48e9420bbe97bea9673a
delallavemartin <delalallave.martin@gmail.com>
2016-08-28 17:53:32 -0300

    Se modifico la respuesta del get de msgs

--- src/main/java/com/velociteam/pspecs/dao/MensajesDao.java
+++ src/main/java/com/velociteam/pspecs/dao/MensajesDao.java
@@ -44,7 +44,7 @@ public class MensajesDao extends AbstractDao{
 			
 			ResponseMsgDTO response = new ResponseMsgDTO(
 					(String) ((DBObject) mensaje).get("usuarioOrigen"),
-					new SimpleDateFormat("dd/MM/yyyy-hh:mm").format((Date) ((DBObject) mensaje).get("timestamp")),
+					(String) ((DBObject) mensaje).get("timestamp"),
 					imagenes);
 			
 			mensajes.add(response);

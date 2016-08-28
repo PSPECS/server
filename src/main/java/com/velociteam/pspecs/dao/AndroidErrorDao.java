022d9f0d83c57e597d122a80f6f99135d40eb592
delallavemartin <delalallave.martin@gmail.com>
2016-08-28 16:56:54 -0300

    Se modifico el Android erro dto

--- src/main/java/com/velociteam/pspecs/dao/AndroidErrorDao.java
+++ src/main/java/com/velociteam/pspecs/dao/AndroidErrorDao.java
@@ -11,8 +11,14 @@ public class AndroidErrorDao extends AbstractDao{
 	public void logError(AndroidErrorDTO error) {
 		super.getDB().getCollection("androidLog").insert(
 				new BasicDBObject("cause",error.getCause())
-				.append("device",error.getDevice())
-				.append("firmware",error.getFirmware()));
+				.append("device",new BasicDBObject("brand",error.getDevice().getBrand())
+						.append("device", error.getDevice().getDevice())
+						.append("id", error.getDevice().getId())
+						.append("model", error.getDevice().getModel())
+						.append("product", error.getDevice().getProduct()))
+				.append("firmware",new BasicDBObject("sdk",error.getFirmware().getSdk())
+						.append("release", error.getFirmware().getRelease())
+						.append("incremental", error.getFirmware().getIncremental())));
 	}
 
 }

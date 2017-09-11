a66bfba1d7c1ec7f8d19bc26704ddfe8769af1e9
delallavemartin <delalallave.martin@gmail.com>
2017-09-10 21:21:08 -0300

    test search

--- src/main/java/com/velociteam/pspecs/services/GoogleAPIService.java
+++ src/main/java/com/velociteam/pspecs/services/GoogleAPIService.java
@@ -16,13 +16,13 @@ public class GoogleAPIService {
 
 	private static String CLIENT_ID = "589899251108-j0m6fff8p74175c3lfibivrpgvfmp9vj.apps.googleusercontent.com";
 	
+	GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+		    .setAudience(Collections.singletonList(CLIENT_ID))
+		    // Or, if multiple clients access the backend:
+		    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+		    .build();
+	
 	public boolean isValidToken(String idTokenString) throws GeneralSecurityException, IOException{
-		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
-			    .setAudience(Collections.singletonList(CLIENT_ID))
-			    // Or, if multiple clients access the backend:
-			    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-			    .build();
-
 			// (Receive idTokenString by HTTPS POST)
 			GoogleIdToken idToken = verifier.verify(idTokenString);
 			return (idToken != null);
